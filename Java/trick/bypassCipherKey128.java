3b1b903217404b6e5b1a963e926e77ed9ba4ca11
skycrab <skycrab68@163.com>
2020-03-13 18:02:27 +0800

    feat(trick) bypss Illegal key size

---
+++ Java/trick/bypassCipherKey128.java
@@ -0,0 +1,41 @@
+
+//修复java.security.InvalidKeyException: Illegal key size。
+
+public static void fixKeyLength() {
+        String errorString = "Failed manually overriding key-length permissions.";
+        int newMaxKeyLength;
+        try {
+            if ((newMaxKeyLength = Cipher.getMaxAllowedKeyLength("AES")) < 256) {
+                Class c = Class.forName("javax.crypto.CryptoAllPermissionCollection");
+                Constructor con = c.getDeclaredConstructor();
+                con.setAccessible(true);
+                Object allPermissionCollection = con.newInstance();
+                Field f = c.getDeclaredField("all_allowed");
+                f.setAccessible(true);
+                f.setBoolean(allPermissionCollection, true);
+
+                c = Class.forName("javax.crypto.CryptoPermissions");
+                con = c.getDeclaredConstructor();
+                con.setAccessible(true);
+                Object allPermissions = con.newInstance();
+                f = c.getDeclaredField("perms");
+                f.setAccessible(true);
+                ((Map) f.get(allPermissions)).put("*", allPermissionCollection);
+
+                c = Class.forName("javax.crypto.JceSecurityManager");
+                f = c.getDeclaredField("defaultPolicy");
+                f.setAccessible(true);
+                Field mf = Field.class.getDeclaredField("modifiers");
+                mf.setAccessible(true);
+                mf.setInt(f, f.getModifiers() & ~Modifier.FINAL);
+                f.set(null, allPermissions);
+
+                newMaxKeyLength = Cipher.getMaxAllowedKeyLength("AES");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(errorString, e);
+        }
+        if (newMaxKeyLength < 256) {
+            throw new RuntimeException(errorString); // hack failed
+        }
+    }
